2ec519c607b0dc6b093346f06cfbb07e9b1f019a
Maria Glazova <m_glazova@yahoo.com>
2019-04-11 01:37:48 +0300

    Добавлено задание к уроку 1

---
+++ core/src/ru/dartusha/game/BaseScreen.java
@@ -0,0 +1,96 @@
+package ru.dartusha.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public abstract class BaseScreen implements Screen, InputProcessor {
+
+    protected SpriteBatch batch;
+    Texture img;
+
+    @Override
+    public void show() {
+        System.out.println("show");
+        Gdx.input.setInputProcessor(this);
+        batch = new SpriteBatch();
+        img = new Texture("background3.jpg");
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 0);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, 0, 0,1024,2048);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+        dispose();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+}
+
--- core/src/ru/dartusha/game/StarGame.java
+++ core/src/ru/dartusha/game/StarGame.java
@@ -1,35 +1,19 @@
 package ru.dartusha.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class StarGame extends ApplicationAdapter {
+public class StarGame extends Game {
 	SpriteBatch batch;
 	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("background3.jpg");
-	}
 
 	@Override
-	//ДЗ1 Подобрать картинку для фона, сделать её размеры кратными степени 2
-	// (например 2048x1024 или 2048x2048) и добавить фон в игру
-	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0,1024,2048);
-		batch.end();
+	public void create() {
+		setScreen(new MenuScreen());
 	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
 }
