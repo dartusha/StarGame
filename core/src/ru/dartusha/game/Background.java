c0e118a19d4c1caa95d143138c95ef3137b7e0f2
Maria Glazova <m_glazova@yahoo.com>
2019-04-14 14:58:26 +0300

    Добавлено задание к уроку 3

---
+++ core/src/ru/dartusha/game/Background.java
@@ -0,0 +1,16 @@
+package ru.dartusha.game;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Background extends Sprite {
+
+    public Background(TextureRegion region) {
+        super(region);
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        setHeightProportion(worldBounds.getHeight());
+        pos.set(worldBounds.pos);
+    }
+}
--- core/src/ru/dartusha/game/MenuScreen.java
+++ core/src/ru/dartusha/game/MenuScreen.java
@@ -1,82 +1,56 @@
 package ru.dartusha.game;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
-
 
 public class MenuScreen extends BaseScreen {
 
-    private Vector2 touch;
-    private Vector2 pos;
-    private Vector2 v;
-    private Texture img;
-
-    private Vector2 test;
+    private Texture bg, shipTexture;
+    private Background background;
+    private Ship ship;
 
     @Override
     public void show() {
         super.show();
-        touch = new Vector2();
-        pos = new Vector2();
-        v = new Vector2(0f,2f);
-        img = new Texture("ship3.png");
+        bg = new Texture("background3.jpg");
+        background = new Background(new TextureRegion(bg));
+        shipTexture=new Texture("ship3.png");
+        ship = new Ship (new TextureRegion(shipTexture));
     }
 
-    /*
-    2.Реализовать движение логотипа badlogic (можно свою картинку вставить)
-    при нажатии кнопки мыши (touchDown) в точку нажатия на экране и остановку в данной точке.
-     */
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+        ship.resize(worldBounds);
+    }
+
     @Override
     public void render(float delta) {
-            super.render(delta);
-        pos.add(v);
+        super.render(delta);
         batch.begin();
-        batch.draw(img, pos.x, pos.y);
+        background.draw(batch);
+        ship.draw(batch);
         batch.end();
-        if ((Math.abs(pos.y-touch.y)<5)&&(Math.abs(pos.x -touch.x)<5)){
-          // System.out.println("stop");
-            v.setZero();
-       }
     }
 
     @Override
     public void dispose() {
         super.dispose();
-        img.dispose();
+        bg.dispose();
+        shipTexture.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
-       // System.out.println("touchDown touch.x = " + touch.x + " touch.y = " + touch.y);
-       // System.out.println("touchDown pos.x = " + pos.x + " pos.y = " + pos.y);
-
-        test=touch.cpy().sub(pos).nor();
-       // System.out.println(test.x+","+test.y);
-        v.set(test.x,test.y);
-        return false;
+    public boolean touchDown(Vector2 touch, int pointer) {
+        ship.touchDown(touch, pointer); return false;
     }
 
     @Override
-    /*
-    3.Реализовать управление логотипом с помощью клавиш со стрелками на клавиатуре*** (дополнительное задание, не обязательное к выполнению)
-     */
     public boolean keyUp(int keycode) {
-        if (keycode==20){
-            pos.y=pos.y-50;
-        }
-        if (keycode==19){
-            pos.y=pos.y+50;
-        }
-        if (keycode==22){
-            pos.x=pos.x+50;
-        }
-        if (keycode==21){
-            pos.x=pos.x-50;
-        }
+        ship.keyMove(keycode);
         return false;
     }
-
 
 }
