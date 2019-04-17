d57712874f7918b3d51083d42f47543e280cfac6
Maria Glazova <m_glazova@yahoo.com>
2019-04-17 10:20:42 +0300

    Добавлено задание к уроку 3

--- core/src/ru/dartusha/game/MenuScreen.java
+++ core/src/ru/dartusha/game/MenuScreen.java
@@ -1,6 +1,7 @@
 package ru.dartusha.game;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
@@ -9,14 +10,24 @@ public class MenuScreen extends BaseScreen {
     private Texture bg, shipTexture;
     private Background background;
     private Ship ship;
+    private TextureAtlas atlas;
+    private Star star;
+    private Star starList[];
 
     @Override
     public void show() {
         super.show();
-        bg = new Texture("background3.jpg");
+        //bg = new Texture("background3.jpg");
+        bg = new Texture("textures/bg.png");
         background = new Background(new TextureRegion(bg));
-        shipTexture=new Texture("ship3.png");
-        ship = new Ship (new TextureRegion(shipTexture));
+       // shipTexture=new Texture("ship3.png");
+       // ship = new Ship (new TextureRegion(shipTexture));
+        atlas=new TextureAtlas("textures/mainAtlas.tpack");
+        ship = new Ship (atlas);
+        starList = new Star[256];
+        for (int i = 0; i < starList.length; i++) {
+            starList[i] = new Star(atlas);
+        }
     }
 
     @Override
@@ -24,22 +35,43 @@ public class MenuScreen extends BaseScreen {
         super.resize(worldBounds);
         background.resize(worldBounds);
         ship.resize(worldBounds);
+        for (Star star : starList) {
+            star.resize(worldBounds);
+        }
+
     }
+
+    private void draw() {
+        batch.begin();
+        background.draw(batch);
+        for (Star star : starList) {
+            star.draw(batch);
+        }
+        ship.draw(batch);
+        batch.end();
+    }
+
+
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        batch.begin();
-        background.draw(batch);
-        ship.draw(batch);
-        batch.end();
+        update(delta);
+        draw();
+    }
+
+    private void update(float delta) {
+        ship.update(delta);
+        for (Star star : starList) {
+            star.update(delta);
+        }
     }
 
     @Override
     public void dispose() {
         super.dispose();
         bg.dispose();
-        shipTexture.dispose();
+        atlas.dispose();
     }
 
     @Override
--- core/src/ru/dartusha/game/Ship.java
+++ core/src/ru/dartusha/game/Ship.java
@@ -1,91 +1,74 @@
 package ru.dartusha.game;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
 
 public class Ship extends Sprite {
 
     private Vector2 v;
+    private Rect worldBounds;
 
     private Vector2 test, destination, point1, point2;
     private float rotation=0;
     float angle=0;
 
 
-    public Ship(TextureRegion region) {
-        super(region);
+    public Ship(TextureAtlas atlas) {
+        super(atlas.findRegion("main_ship"));
         v = new Vector2(0f,0f);
-        point1 = new Vector2(0, halfHeight);
+        point1 = new Vector2(0, 0);
         point2 = new Vector2(0, 0);
-
+        test = new Vector2(0, 0);
+        setHeightProportion(0.1f);
     }
 
     @Override
     public void resize(Rect worldBounds) {
-        setHeightProportion(worldBounds.getHeight()/10);
+        this.worldBounds=worldBounds;
+        super.resize(worldBounds);
         pos.set(worldBounds.pos);
     }
 
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer) {
+        point1=pos;
 
         v.set(0f,0.001f);
         destination=touch;
 
+        point1=point2;
         test=touch.cpy().sub(pos).nor();
         v.set(test.x/1000,test.y/1000);
-
-        point1.set(0, touch.len());
-        point2.set(touch.x, touch.y);
-
-        angle = point1.angle(point2);
-
-        rotation=0;
-        rotation=angle;
-
         return false;
     }
 
-
-    public void draw(SpriteBatch batch) {
+    @Override
+    public void update(float delta) {
+        super.update(delta);
         pos.add(v);
         if (v.y!=0){
             if ((Math.abs(pos.y-destination.y)<0.005)&&(Math.abs(pos.x -destination.x)<0.005)) {
                 v.setZero();
             }
-            }
-        batch.draw(
-                regions[frame],
-                getLeft(), getBottom(),
-                halfWidth, halfHeight,
-                getWidth(), getHeight(),
-                scale, scale,rotation
-               // angle
-
-        );
+        }
     }
+
 
     public boolean keyMove(int keycode) {
         angle=0;
 
         if (keycode==20){
-            pos.y=pos.y-0.01f;
-            rotation=180;
+            pos.y=pos.y-0.02f;
         }
         if (keycode==19){
-            pos.y=pos.y+0.01f;
-            rotation=0;
+            pos.y=pos.y+0.02f;
         }
         if (keycode==22){
-            pos.x=pos.x+0.01f;
-            rotation=-90;
+            pos.x=pos.x+0.02f;
         }
         if (keycode==21){
-            pos.x=pos.x-0.01f;
-            rotation=90;
+            pos.x=pos.x-0.02f;
         }
         return false;
     }
